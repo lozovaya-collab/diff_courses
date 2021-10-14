6fde7a9b7d98173b19f280e28e28b1804a615ff7
Anya Lozovaya <Lozovaya.A@m-style.spb.ru>
2021-10-14 11:16:51 +0300

    feat: add project from Vladilen course:: Express js

---
+++ node_express/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import path from "path"
+import { requestTime, logger } from "./middlewares.js"
+import serverRoutes from "./routes/servers.js"
+
+const __dirname = path.resolve()
+const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "ejs"))
+
+const PORT = process.env.PORT || 3000
+
+app.use(requestTime) // регистрируем
+app.use(logger)
+app.use(serverRoutes)
+
+// работаем с шаблонизатором ejs
+app.get("/", (req, res) => {
+    res.render("index", { title: "Main Page" })
+})
+
+app.get("/features", (req, res) => {
+    res.render("features", { title: "Features Page" })
+})
+
+app.listen(3000)
